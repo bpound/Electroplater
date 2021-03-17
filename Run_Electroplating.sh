41699069475890cf5fcc76bf48aa6266d3ad0bc1
bpound <80844673+bpound@users.noreply.github.com>
2021-03-17 15:43:57 -0700

    Delete Run_Electroplating.sh

--- Run_Electroplating.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "/home/pi/Desktop/electroplating_source"
-source "electroplating_source/bin/activate"
-python3 "Electroplating_code.py"
-deactivate
-echo "\nClosing window in 10 seconds."
-sleep 10
